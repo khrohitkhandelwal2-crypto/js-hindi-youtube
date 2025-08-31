84fd96e1e17001ded25263e413e56a8b2c13edf8
Rohit Khandelwal <khrohitkhandelwal2@gmail.com>
2025-08-31 11:48:34 +0000

    data_type

--- .devcontainer/01_basics/day_01.js
+++ .devcontainer/01_basics/day_01.js
@@ -1,1 +1,15 @@
 console.log("rohit khandelwal")
+const accountId =112233  // change nhi kiya jata
+let accountEmail = "khrohitkhandelwal@gmail.com"  //
+var accountPassword ="123456"  //prefer not to use var because of issue in block scope and functional scope 
+accountCity ="jaipur"
+let accountState;
+// accountId = 2  //not allowed
+
+// accountEmail ="hc@hc.com"
+// accountPassword = "212121211"
+// accountCity = "bengaluru"
+
+console.log( accountId)
+
+console.table([accountId , accountEmail , accountPassword, accountCity ,accountState])
