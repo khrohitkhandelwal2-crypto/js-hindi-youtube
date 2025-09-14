875466c64775a1d2f954e7dc459e9f297c70e9ca
Rohit Khandelwal <khrohitkhandelwal2@gmail.com>
2025-09-14 17:26:07 +0000

    math

---
+++ .devcontainer/01_basics/day_05.js
@@ -0,0 +1,36 @@
+ const score = 400
+//  console.log(typeof(score));
+ 
+ const balance =new Number(100)
+//  console.log(balance);
+//  console.log(balance.toString() .length);
+//   console.log(balance.toFixed(2));
+ 
+  const otherNumber =23.5646
+//   console.log(otherNumber.toPrecision(5));
+
+//   const hunderds =10000000
+//   console.log(hunderds.toLocaleString('en-IN'));
+
+//   +++++++++++++++MATHS+++++++++++++++++++++
+  console.log(Math);
+  
+    console.log(Math.abs(-45));
+  console.log(Math.round(45.265));
+
+    console.log(Math.ceil(45.265));
+    
+    console.log(Math.floor(45.265));
+
+        console.log(Math.min(4,5.2,6,5)); 
+        console.log(Math.max(4,5.2,6,5));
+
+                console.log(Math.random());
+                 console.log((Math.random()*10)+1);
+                 console.log(Math.floor(Math.random()*10)+1);
+                 
+          
+const min=10
+const max=50
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
